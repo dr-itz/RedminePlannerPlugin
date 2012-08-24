fb10b7bee2d0795301dd1867d8622feececbad9a
Daniel Ritz <daniel.ritz@gmx.ch>
2012-08-24 21:35:57 +0200

    Initial import of empty plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :planner do
+  name 'Planner plugin'
+  author 'Daniel Ritz'
+  description 'Redmine Resource Planner Plugin'
+  version '0.0.1'
+  url 'http://github.com/dr-itz/RedminePlannerPlugin'
+  author_url 'mailto:daniel.ritz@gmx.ch'
+end
