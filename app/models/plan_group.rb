0988577b28689dcf394716e66f543fb1d0e44ed9
Daniel Ritz <daniel.ritz@gmx.ch>
2012-09-02 16:21:02 +0200

    Teams/groups: clean up group_type
    Refs #120

--- app/models/plan_group.rb
+++ app/models/plan_group.rb
@@ -1,10 +1,26 @@
 class PlanGroup < ActiveRecord::Base
   unloadable
 
+  include Redmine::I18n
+
   belongs_to :project
   belongs_to :team_leader, :class_name => 'User', :foreign_key => 'leader_id'
 
+  TYPE_TEAM = 1
+  TYPE_GROUP = 2
+
+  # Returns all PlanGroups belonging to the specified +project+
   def self.all_project_groups(project)
     self.where(:project_id => project.is_a?(Project) ? project.id : project).order(:name)
+  end
+
+  # Returns an array of group types for ERB select
+  def self.group_types_select
+    [[ l(:label_planner_group_team), TYPE_TEAM ], [ l(:label_planner_group_group), TYPE_GROUP ]]
+  end
+
+  # Returns the group_type as i18n string
+  def type_string
+    group_type == TYPE_TEAM ? l(:label_planner_group_team) : l(:label_planner_group_group)
   end
 end
