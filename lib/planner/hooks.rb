371950cbc2c82ad132748a9cbc9c86fcfcaf4f25
Daniel Ritz <daniel.ritz@gmx.ch>
2012-09-27 02:51:19 +0200

    Always load planner stylesheet via hook for simpler handling

--- lib/planner/hooks.rb
+++ lib/planner/hooks.rb
@@ -1,12 +1,12 @@
 module RedminePlanner
   class Hooks < Redmine::Hook::ViewListener
     def view_layouts_base_html_head(context)
+      o = stylesheet_link_tag('planner', :plugin => 'planner')
       if Redmine::VERSION::MAJOR == 2 && Redmine::VERSION::MINOR < 1
-        javascript_include_tag("jquery-1.7.2.min.js", :plugin => :planner) +
+        o += javascript_include_tag("jquery-1.7.2.min.js", :plugin => :planner) +
           %(<script type="text/javascript">//<![CDATA[\njQuery.noConflict();\n//]]></script>).html_safe
-      else
-        ""
       end
+      return o
     end
   end
 end
