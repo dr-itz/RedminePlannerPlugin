10081604df1daf1fa7ae5314f516cef992a8cdb6
Daniel Ritz <daniel.ritz@gmx.ch>
2012-10-07 00:30:27 +0200

    Charts: make displayed range selectable
    Refs #131

--- app/controllers/planner_controller.rb
+++ app/controllers/planner_controller.rb
@@ -7,8 +7,17 @@ class PlannerController < ApplicationController
   end
 
   def show_user
+    start_date = params[:week_start_date]
+    if start_date
+      @start_date = Date.parse(start_date)
+    else
+      @start_date = Date.today
+    end
+    num_weeks = params[:num_weeks]
+    @num_weeks = (num_weeks || 8).to_i
+
     @user = User.find(params[:id])
     @chart = PlanChart.new
-    @chart.generate_user_chart(@user, Date.today, 8)
+    @chart.generate_user_chart(@user, @start_date, @num_weeks)
   end
 end
