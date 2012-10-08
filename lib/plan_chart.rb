f29c3f7e53bfe577489943616b9edef7796e9332
Daniel Ritz <daniel.ritz@gmx.ch>
2012-10-08 17:50:23 +0200

    Charts: nicer colors, from D3.js
    Refs #128, #129

--- lib/plan_chart.rb
+++ lib/plan_chart.rb
@@ -8,6 +8,24 @@ class PlanChart
   attr_reader :weeks, :ticks, :data, :series, :start_date, :end_date,
     :max, :limit, :width, :height, :tick_interval
 
+  # d3_category20
+  #COLORS = [
+  #  "#1f77b4", "#aec7e8", "#ff7f0e", "#ffbb78", "#2ca02c", "#98df8a", "#d62728",
+  #  "#ff9896", "#9467bd", "#c5b0d5", "#8c564b", "#c49c94", "#e377c2", "#f7b6d2",
+  #  "#7f7f7f", "#c7c7c7", "#bcbd22", "#dbdb8d", "#17becf", "#9edae5" ];
+
+  # d3_category20b
+  #COLORS = [
+  #  "#393b79", "#5254a3", "#6b6ecf", "#9c9ede", "#637939", "#8ca252", "#b5cf6b",
+  #  "#cedb9c", "#8c6d31", "#bd9e39", "#e7ba52", "#e7cb94", "#843c39", "#ad494a",
+  #  "#d6616b", "#e7969c", "#7b4173", "#a55194", "#ce6dbd", "#de9ed6" ];
+
+  # d3_category20c
+  COLORS = [
+    "#3182bd", "#6baed6", "#9ecae1", "#c6dbef", "#e6550d", "#fd8d3c", "#fdae6b",
+    "#fdd0a2", "#31a354", "#74c476", "#a1d99b", "#c7e9c0", "#756bb1", "#9e9ac8",
+    "#bcbddc", "#dadaeb", "#636363", "#969696", "#bdbdbd", "#d9d9d9" ];
+
   def generate_user_chart(user, start_date, weeks)
     setup_chart start_date, weeks
     @limit = 100
@@ -31,6 +49,7 @@ class PlanChart
       request = PlanRequest.find(req, :include => :task)
       @series[i] = {}
       @series[i][:label] = l(:label_planner_request_short) + " #" + req.to_s + ": " + request.task.name
+      @series[i][:color] = get_color i
     end
 
     check_empty
@@ -59,11 +78,14 @@ class PlanChart
       resource = User.find(res)
       @series[i] = {}
       @series[i][:label] = resource.name
+      @series[i][:color] = get_color i
     end
 
     check_empty
   end
+
 private
+
   def normalize_date(date)
     Date.commercial(date.cwyear, date.cweek, 1)
   end
@@ -93,5 +115,9 @@ private
     unless @data.any?
       @data.push Array.new(weeks, 0)
     end
+  end
+
+  def get_color(i)
+    COLORS[i % COLORS.length]
   end
 end
