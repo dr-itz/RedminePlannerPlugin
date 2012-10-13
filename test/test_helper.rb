7af45b19405945e76bab92be5cd674c31a611738
Daniel Ritz <daniel.ritz@gmx.ch>
2012-10-13 18:04:57 +0200

    SimpleCov and annotate, working with Ruby 1.9.3

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,20 @@
+if RUBY_VERSION > "1.8"
+  require 'simplecov'
+
+  if Dir.pwd.match(/plugins\/planner/)
+    covdir = 'coverage'
+  else
+    covdir = 'plugins/planner/coverage'
+  end
+
+  SimpleCov.coverage_dir(covdir)
+  SimpleCov.start 'rails' do
+    add_filter do |source_file|
+      source_file.filename.match(/planner/) == nil
+    end
+  end
+end
+
 # Load the normal Redmine helper
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
 
