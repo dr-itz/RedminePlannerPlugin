5d1c3106c38ce7a91d8fe7880e58dc76b794f0e2
Daniel Ritz <daniel.ritz@gmx.ch>
2012-11-15 01:11:50 +0100

    Remove CDATA crap from hook

--- lib/planner/hooks.rb
+++ lib/planner/hooks.rb
@@ -4,7 +4,7 @@ module RedminePlanner
       o = stylesheet_link_tag('planner', :plugin => 'planner')
       if Redmine::VERSION::MAJOR == 2 && Redmine::VERSION::MINOR < 1
         o += javascript_include_tag("jquery-1.7.2.min.js", :plugin => :planner) +
-          %(<script type="text/javascript">//<![CDATA[\njQuery.noConflict();\n//]]></script>).html_safe
+          %(<script type="text/javascript">jQuery.noConflict();</script>).html_safe
       end
       o += javascript_include_tag("planner", :plugin => :planner)
       return o
