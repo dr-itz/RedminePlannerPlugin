b313e3c8bf4acfe95f1efd4fc31a2abc041786be
Daniel Ritz <daniel.ritz@gmx.ch>
2012-11-30 16:46:11 +0100

    Charts: include display range for task chart
    Refs #130

--- app/helpers/planner_helper.rb
+++ app/helpers/planner_helper.rb
@@ -4,12 +4,12 @@ module PlannerHelper
     link_to name, planner_show_user_path(project, user, opts)
   end
 
-  def link_to_group_plan_view(project, group)
+  def link_to_group_plan_view(project, group, opts = {})
     name = h(group.name)
-    link_to name, planner_show_group_path(project, group)
+    link_to name, planner_show_group_path(project, group, opts)
   end
 
-  def link_to_task_plan_view(task)
-    link_to task.name, planner_show_task_path(task.project, task)
+  def link_to_task_plan_view(task, opts = {})
+    link_to task.name, planner_show_task_path(task.project, task, opts)
   end
 end
