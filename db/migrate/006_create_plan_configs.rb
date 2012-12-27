896b398177ee4310db4c23232b0c949aec546ef0
Daniel Ritz <daniel.ritz@gmx.ch>
2012-12-27 10:53:33 +0100

    Fix migration

--- db/migrate/006_create_plan_configs.rb
+++ db/migrate/006_create_plan_configs.rb
@@ -1,10 +1,10 @@
-class CreatePlanConfig < ActiveRecord::Migration
+class CreatePlanConfigs < ActiveRecord::Migration
   def change
     create_table :plan_configs do |t|
       t.integer :project_id,       :default => 0,     :null => false
       t.string  :config,                              :null => false
     end
 
-    add_index "plan_config", ["project_id"], :name => "plan_config_project_id"
+    add_index "plan_configs", ["project_id"], :name => "plan_configs_project_id"
   end
 end
